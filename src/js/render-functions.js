aae6acee2a07fd6c52eadf1caf19eff2f4d44d4f
Roman Isko <roman_isko@ukr.net>
2025-05-10 15:29:05 +0300

    modified

---
+++ src/js/render-functions.js
@@ -0,0 +1,38 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      img => `
+      <a href="${img.largeImageURL}">
+        <img src="${img.webformatURL}" alt="${img.tags}" />
+        <div>
+          <p>Likes: ${img.likes}</p>
+          <p>Views: ${img.views}</p>
+          <p>Comments: ${img.comments}</p>
+          <p>Downloads: ${img.downloads}</p>
+        </div>
+      </a>`
+    )
+    .join('');
+  galleryContainer.innerHTML = markup;
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryContainer.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.add('visible');
+}
+
+export function hideLoader() {
+  loader.classList.remove('visible');
+}
